42c1b65fad70255148450886501e3d66e6a62744
6x1 <137667160+sixbyone@users.noreply.github.com>
2023-06-28 07:09:25 -0700

    Add Java support
    Signed-off-by: 6x1 <137667160+sixbyone@users.noreply.github.com>

---
+++ config/fish/functions/java_env.fish
@@ -0,0 +1,14 @@
+function java_env
+  if command -s /usr/libexec/java_home > /dev/null 
+    if test (count $argv) -eq 0
+       /usr/libexec/java_home -V
+    else
+      switch $argv[1]
+      case 'ls'
+        /usr/libexec/java_home -V
+      case 'set'
+        set -xU JAVA_HOME (/usr/libexec/java_home -v $argv[2])
+      end
+    end
+  end
+end
