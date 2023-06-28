e5800a76f0525a364e38b2853c74abcc884e6b1b
6x1 <137667160+sixbyone@users.noreply.github.com>
2023-06-28 07:09:25 -0700

    Check to see if 'omf' is installed before sourcing init file
    Signed-off-by: 6x1 <137667160+sixbyone@users.noreply.github.com>

--- config/fish/conf.d/1-omf.fish
+++ config/fish/conf.d/1-omf.fish
@@ -1,7 +1,9 @@
 # Path to Oh My Fish install.
-  set -q XDG_DATA_HOME
+set -q XDG_DATA_HOME
     and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
     or set -gx OMF_PATH "$HOME/.local/share/omf"
 
-  # Load Oh My Fish configuration.
+# Load Oh My Fish configuration.
+if command -s $OMF_PATH/init.fish > /dev/null
   source $OMF_PATH/init.fish
+end
