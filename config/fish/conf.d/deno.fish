97df62d63dee71f1c69866d82016c66550e6d971
6x1 <137667160+sixbyone@users.noreply.github.com>
2023-08-02 12:40:17 -0700

    deno config Linux fixes

--- config/fish/conf.d/deno.fish
+++ config/fish/conf.d/deno.fish
@@ -1,7 +1,14 @@
-# Add any standard paths
+#
+# Add paths and environment variables if deno is installed
+#
 if status is-login
   if command -s deno > /dev/null
+    # Add the deno bin directory
     fish_add_path ~/.deno/bin
-    set -x DENO_DEPLOY_TOKEN (security find-generic-password -a "deno-deploy" -w)
+
+    # Add the deno deploy token to the environment
+    if command -s security > /dev/null
+      set -x DENO_DEPLOY_TOKEN (security find-generic-password -a "deno-deploy" -w)
+    end
   end
 end
